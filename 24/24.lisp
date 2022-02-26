c8ec2acce1cd38f37cfcec662c61138b711ff82f
Paul Fuhrmann <mail@paulfuhrmann.de>
2022-02-26 18:06:31 +0100

    Added stubs for Day 24

---
+++ 24/24.lisp
@@ -0,0 +1,12 @@
+(in-package #:aoc2021)
+
+
+(defun 24-solve-1 (&optional (path "24/24.txt"))
+  (declare (ignore path))
+  (warn "Part 1 of day 24 is not solved yet!"))
+
+
+(defun 24-solve-2 (&optional (path "24/24.txt"))
+  (declare (ignore path))
+  (warn "Part 2 of day 24 is not solved yet!"))
+
